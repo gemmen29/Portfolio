3594bc45f71405d4adb37921fe46e83a16003fc4
George Magdy <georgtriple@gmail.com>
2021-11-08 15:02:56 +0200

    Fix infinte loop problem

--- index.js
+++ index.js
@@ -1,21 +1,25 @@
-const menuIcon = document.getElementById('menu-icon');
-const menuList = document.getElementById('menu-list');
-const closeMenu = document.querySelectorAll('#menu-list ul li a');
+window.addEventListener("DOMContentLoaded", () => {
+  const menuIcon = document.getElementById("menu-icon");
+  const menuList = document.getElementById("menu-list");
+  const closeMenu = document.querySelectorAll("#menu-list ul li a");
 
-const allBlur = document.getElementsByClassName('add-blur');
+  const allBlur = document.getElementsByClassName("add-blur");
 
-menuIcon.addEventListener('click', () => {
-  menuList.classList.remove('d-none');
-  for (let index = 0; index < allBlur.length; index += index) {
-    allBlur[index].classList.add('blur');
-  }
-});
-
-for (let index = 0; index < closeMenu.length; index += index) {
-  closeMenu[index].addEventListener('click', () => {
-    menuList.classList.add('d-none');
-    for (let index = 0; index < allBlur.length; index += index) {
-      allBlur[index].classList.remove('blur');
+  menuIcon.addEventListener("click", () => {
+    menuList.classList.remove("d-none");
+    for (let index = 0; index < allBlur.length; index += 1) {
+      allBlur[index].classList.add("blur");
     }
   });
-}
+
+  for (let index = 0; index < closeMenu.length; index += 1) {
+    closeMenu[index].addEventListener("click", () => {
+      menuList.classList.add("d-none");
+      for (let i = 0; i < allBlur.length; i += 1) {
+        allBlur[i].classList.remove("blur");
+      }
+    });
+  }
+  console.log(closeMenu);
+  console.log(closeMenu.length);
+});
