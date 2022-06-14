c1eec8b218a1b27369a3dd9327235c93e20168ed
George Magdy <georgtriple@gmail.com>
2022-06-14 14:11:33 +0200

    Update projects

---
+++ js/projects.js
@@ -0,0 +1,56 @@
+const projects = [
+  {
+    name: 'Budget App',
+    description:
+      'The Budget app is a mobile web application where you can manage your budget: you have a list of transactions associated with a category, so that you can see how much money you spent and on what.',
+    featuredImage: 'images/budget.png',
+    technologies: ['ruby', 'ruby on rails', 'postgres', 'tailwindcss'],
+    liveVersion: 'https://arcane-woodland-30814.herokuapp.com',
+    source: 'https://github.com/gemmen29/budget-app',
+  },
+  {
+    name: 'Space Xplore',
+    description:
+      'Space Xplore is a React-Redux application consists of Rockets, Missions, and the My Profile section',
+    featuredImage: 'images/space.jpg',
+    technologies: ['react', 'redux', 'tailwindcss'],
+    liveVersion: 'https://space-xplore-500.netlify.app',
+    source: 'https://github.com/gemmen29/Space-Xplore',
+  },
+  {
+    name: 'Show app',
+    description:
+      'Website about online course academy built with HTML, CSS, and Javascript',
+    featuredImage: 'images/show.png',
+    technologies: ['html', 'css', 'javascript', 'bootstrap', 'sass', 'api'],
+    liveVersion: 'https://gemmen29.github.io/show-app/',
+    source: 'https://github.com/gemmen29/show-app',
+  },
+  {
+    name: 'Online Courses Website',
+    description:
+      'Website about online course academy built with HTML, CSS, and Javascript',
+    featuredImage: 'images/capstone.png',
+    technologies: ['html', 'css', 'javascript', 'bootstrap'],
+    liveVersion: 'https://gemmen29.github.io/Online-Courses-Website/',
+    source: 'https://github.com/gemmen29/Online-Courses-Website',
+  },
+  {
+    name: 'Awesome Books',
+    description: 'Basic website for add and remove books',
+    featuredImage: 'images/awesome.png',
+    technologies: ['html', 'css', 'javascript', 'bootstrap'],
+    liveVersion: 'https://gemmen29.github.io/Awesome-Book/',
+    source: 'https://github.com/gemmen29/Awesome-Book',
+  },
+  {
+    name: 'ToDo List',
+    description: 'This is a website for managing the daily tasks',
+    featuredImage: 'images/todo.png',
+    technologies: ['html', 'css', 'javascript', 'webpacks', 'jest'],
+    liveVersion: 'https://gemmen29.github.io/ToDo-List/dist/',
+    source: 'https://github.com/gemmen29/ToDo-List',
+  },
+];
+
+export default projects;
