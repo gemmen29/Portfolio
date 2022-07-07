def58d58a6662f56177a6ebfdd99d788e00e38a5
George Magdy <georgtriple@gmail.com>
2022-07-07 09:33:01 +0200

    Add tools list

---
+++ js/tools.js
@@ -0,0 +1,28 @@
+const tools = [
+  {
+    name: 'Git',
+    image: 'images/tools/git.png',
+  },
+  {
+    name: 'VSCode',
+    image: 'images/tools/vs.png',
+  },
+  {
+    name: 'Webpack',
+    image: 'images/tools/webpack.png',
+  },
+  {
+    name: 'Heroku',
+    image: 'images/tools/heroku.png',
+  },
+  {
+    name: 'Netlify',
+    image: 'images/tools/netlify.png',
+  },
+  {
+    name: 'Jest',
+    image: 'images/tools/jest.png',
+  },
+];
+
+export default tools;
