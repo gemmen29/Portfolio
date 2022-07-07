fbd70c98f433809f74e7a5da51993b2564ad561f
George Magdy <georgtriple@gmail.com>
2022-07-07 09:32:49 +0200

    Add frontend list

---
+++ js/front-end.js
@@ -0,0 +1,32 @@
+const frontendTechnologies = [
+  {
+    name: 'HTML',
+    image: 'images/frontend/html5.png',
+  },
+  {
+    name: 'CSS',
+    image: 'images/frontend/css3.png',
+  },
+  {
+    name: 'React',
+    image: 'images/frontend/react.png',
+  },
+  {
+    name: 'Redux',
+    image: 'images/frontend/redux.png',
+  },
+  {
+    name: 'Bootstrap',
+    image: 'images/frontend/bootstrap.png',
+  },
+  {
+    name: 'TailwindCSS',
+    image: 'images/frontend/tailwindcss.png',
+  },
+  {
+    name: 'Sass',
+    image: 'images/frontend/sass.png',
+  },
+];
+
+export default frontendTechnologies;
