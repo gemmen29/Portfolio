3a690c10749b44b02439cdc92917a64dfa9ac0d5
George Magdy <georgtriple@gmail.com>
2022-07-07 09:32:35 +0200

    Add backend list

---
+++ js/back-end.js
@@ -0,0 +1,40 @@
+const backendTechnologies = [
+  {
+    name: 'Ruby',
+    image: 'images/backend/ruby.png',
+  },
+  {
+    name: 'Ruby on Rails',
+    image: 'images/backend/rails.png',
+  },
+  {
+    name: 'PostgreSQL',
+    image: 'images/backend/postgresql.png',
+  },
+  {
+    name: 'NodeJS',
+    image: 'images/backend/nodejs.png',
+  },
+  {
+    name: 'ExpressJS',
+    image: 'images/backend/expressjs.png',
+  },
+  {
+    name: 'MongoDB',
+    image: 'images/backend/mongodb.png',
+  },
+  {
+    name: 'C#',
+    image: 'images/backend/csharp.png',
+  },
+  {
+    name: '.Net',
+    image: 'images/backend/dotnet.png',
+  },
+  {
+    name: 'Sql Server',
+    image: 'images/backend/sqlserver.png',
+  },
+];
+
+export default backendTechnologies;
